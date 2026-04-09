8078ecd927d8ae519c5feb73bcc20a984a971671
jeffjks <jeff_jks@naver.com>
2026-04-09 18:44:34 +0900

    EditPanelView 분리

---
+++ McStructureNbtEditor/Views/EditPanelView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace McStructureNbtEditor.Views
+{
+    public partial class EditPanelView : UserControl
+    {
+        public EditPanelView()
+        {
+            InitializeComponent();
+        }
+    }
+}
